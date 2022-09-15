61eb93885101a323497724d1a32f0906c3440aed
R V <2096tester@gmail.com>
2022-09-15 16:56:30 -0700

    remult database_url

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -2,9 +2,17 @@
 
 import { remultFresh } from "remult/remult-fresh";
 import { Task } from "../model/task.ts";
+import { createPostgresConnection } from "https://deno.land/x/remult/postgres.ts";
 
 export const remultServer = remultFresh({
-    entities: [Task]
- }, Response);
-
-export const handler = remultServer.handle;
+    entities: [Task],
+    dataProvider: async () => {
+      const dbUrl = Deno.env.get("DATABASE_URL");
+      if (dbUrl) {
+        return createPostgresConnection({ connectionString: dbUrl });
+      }
+      return await undefined;
+    },
+  }, Response);
+  
+  export const handler = remultServer.handle;
